ae8ea96d43a847f681df6955d920f0c1e079836f
Ankit Luthra <95644464+aluthra999@users.noreply.github.com>
2023-08-12 00:33:40 -0300

    Add files via upload

---
+++ Day 31 - Capstone Project - French to English Flash Card/main.py
@@ -0,0 +1,52 @@
+from tkinter import *
+import pandas
+import random
+
+BACKGROUND_COLOR = "#B1DDC6"
+
+data = pandas.read_csv("data/french_words.csv")
+to_learn = data.to_dict(orient="records")
+current_card = {}
+
+
+def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(canvas_image, image=card_front_img)
+    flip_timer = window.after(3000, func=flip_card)
+
+
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(canvas_image, image=card_back_img)
+
+
+window = Tk()
+window.title("French to English")
+window.config(padx=50, pady=50, background=BACKGROUND_COLOR)
+
+flip_timer = window.after(3000, func=flip_card)
+
+canvas = Canvas(height=526, width=800, highlightthickness=0, background=BACKGROUND_COLOR)
+card_front_img = PhotoImage(file="./images/card_front.png")
+card_back_img = PhotoImage(file="./images/card_back.png")
+canvas_image = canvas.create_image(400, 263, image=card_front_img)
+card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
+canvas.grid(row=0, column=0, columnspan=2)
+
+wrong_img = PhotoImage(file="./images/wrong.png")
+unknown_button = Button(image=wrong_img, highlightthickness=0, borderwidth=0, command=next_card)
+unknown_button.grid(row=1, column=0)
+
+right_img = PhotoImage(file="./images/right.png")
+known_button = Button(image=right_img, highlightthickness=0, borderwidth=0, command=next_card)
+known_button.grid(row=1, column=1)
+
+next_card()
+
+window.mainloop()
