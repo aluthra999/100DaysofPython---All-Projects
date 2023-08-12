0ecc6cbf0759cf3571a1dbfbadb2fc9c6fdfcdec
Ankit Luthra <95644464+aluthra999@users.noreply.github.com>
2023-08-12 01:11:55 -0300

    Add files via upload

--- Day 31 - Capstone Project - French to English Flash Card/main.py
+++ Day 31 - Capstone Project - French to English Flash Card/main.py
@@ -3,10 +3,15 @@ import pandas
 import random
 
 BACKGROUND_COLOR = "#B1DDC6"
-
-data = pandas.read_csv("data/french_words.csv")
-to_learn = data.to_dict(orient="records")
 current_card = {}
+
+try:
+    data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("data/french_words.csv")
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
 
 
 def next_card():
@@ -23,6 +28,13 @@ def flip_card():
     canvas.itemconfig(card_title, text="English", fill="white")
     canvas.itemconfig(card_word, text=current_card["English"], fill="white")
     canvas.itemconfig(canvas_image, image=card_back_img)
+
+
+def is_known():
+    to_learn.remove(current_card)
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("./data/words_to_learn.csv", index=False)
+    next_card()
 
 
 window = Tk()
@@ -44,7 +56,7 @@ unknown_button = Button(image=wrong_img, highlightthickness=0, borderwidth=0, co
 unknown_button.grid(row=1, column=0)
 
 right_img = PhotoImage(file="./images/right.png")
-known_button = Button(image=right_img, highlightthickness=0, borderwidth=0, command=next_card)
+known_button = Button(image=right_img, highlightthickness=0, borderwidth=0, command=is_known)
 known_button.grid(row=1, column=1)
 
 next_card()
